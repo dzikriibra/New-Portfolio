5d1afbf2f84f073c220c989b3857caf819706015
jikrinichol <wahhab.dzikri@gmail.com>
2026-05-17 03:37:13 +0700

    clean structure directory n path for image profile

--- src/app/modules/about/constants/about.ts
+++ src/app/modules/about/constants/about.ts
@@ -2,7 +2,7 @@ import { Code2, Globe, GraduationCap } from "lucide-react";
 
 export const ABOUT_DATA = {
   typeWriterItems: [" Clarity", " Structure", " Precision", " Consistency"],
-  profileImage: "/BDD.jpg",
+  profileImage: "/profile/my-picture.jpg",
   stats: [
     {
       label: "Years of Experience",
@@ -13,14 +13,14 @@ export const ABOUT_DATA = {
     },
     {
       label: "Project Completed",
-      value: "12+",
+      value: "8+",
       icon: Code2,
       color: "text-green-600",
       hoverBorder: "hover:border-green-700",
     },
     {
       label: "Certificate",
-      value: "8+",
+      value: "11+",
       icon: GraduationCap,
       color: "text-amber-400",
       hoverBorder: "hover:border-amber-400",
